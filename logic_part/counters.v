5eef265c72e836a406326270404f25935c35ffc5
DESKTOP-30LET5T\51140 <machinestalex.lee@gmail.com>
2017-07-07 22:29:47 -0400

    logic has been seperated to file counters and pointer

--- logic_part/counters.v
+++ logic_part/counters.v
@@ -1,19 +1,3 @@
-module pointer(inx, iny, reset, loca);
-  input inx, iny, reset;
-  output [7:0] loca;
-  reg [7:0] loc;
-  reg temp;
-  always @(posedge inx, posedge iny, posedge reset) begin
-    if (reset == 1'b0)
-      loc = 8'b0;
-    else if (inx == 1'b1)
-      loc[3:0] = loc[3:0] + 1'b1;
-    else if (iny == 1'b1)
-      loc[7:4] = loc[7:4] + 1'b1;
-  end
-  assign loca = loc;
-endmodule
-
 module horizontal_check(reset, active, active_next, address, pointer, chess, success, clk, currstate);
   input active, reset;
   input [7:0] pointer;
---
+++ logic_part/pointer.v
@@ -0,0 +1,15 @@
+module pointer(inx, iny, reset, loca);
+  input inx, iny, reset;
+  output [7:0] loca;
+  reg [7:0] loc;
+  reg temp;
+  always @(posedge inx, posedge iny, posedge reset) begin
+    if (reset == 1'b0)
+      loc = 8'b0;
+    else if (inx == 1'b1)
+      loc[3:0] = loc[3:0] + 1'b1;
+    else if (iny == 1'b1)
+      loc[7:4] = loc[7:4] + 1'b1;
+  end
+  assign loca = loc;
+endmodule
