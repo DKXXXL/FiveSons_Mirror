5f8baaf7696bece3a520f4688d27d679f32bf6b6
Shuhao Li <lishuha2@IITS-B473-36.utsc-labs.utoronto.ca>
2017-07-24 18:17:42 -0400

    debug on

--- indebug_pointer/Coordinate_Generate/Coordi_Gener.v
+++
@@ -1,9 +0,0 @@
-module Coordi_Gener(right, down, control_set, resetn, out);
-    input right, down;
-    input resetn, control_set;
-    output [7:0] out;
-
-    X_Coordi_Gener xcoodi(.down(down), .control_set(control_set), .resetn(resetn), .out(out[7:4]));
-    Y_Coordi_Gener ycoodi(.right(right), .control_set(control_set), .resetn(resetn), .out(out[3:0]));
-
-endmodule
--- lowlevelabs/RAM/ram1122x3_inst.v
+++
@@ -1,7 +0,0 @@
-ram1122x3	ram1122x3_inst (
-	.address ( address_sig ),
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.wren ( wren_sig ),
-	.q ( q_sig )
-	);
